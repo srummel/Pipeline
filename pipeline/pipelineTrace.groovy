5e6547e36bb72952268a8b65746877fd59601654
Shawn Rummel <shawn.rummel@gmail.com>
2017-01-26 15:21:05 -0600

    Added Tracing

---
+++ pipeline/pipelineTrace.groovy
@@ -0,0 +1,42 @@
+
+def startStage(location, buildNumber, buildEnvironment, stage){
+    echo "** pipelineTrace.startStage::" + stage + "::Start **"
+
+	File f = new File(location + "/PAC.log")
+	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+    f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::Begin " + stage + '\r\n')
+	
+    echo "** pipelineTrace.startStage::" + stage + "::Finish **"
+}
+
+
+def endStage(location, buildNumber, buildEnvironment, stage){
+    echo "** pipelineTrace.endStage::" + stage + "::Start **"
+
+	File f = new File(location + "/PAC.log")
+	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+    f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::Complete " + stage + '\r\n')
+	
+    echo "** pipelineTrace.endStage::" + stage + "::Finish **"
+}
+
+def output(text){
+    echo "** pipelineTrace.endStage::" + stage + "::Start **"
+
+	File f = new File(location + "/PAC.log")
+    f.append(text + '\r\n')
+	
+    echo "** pipelineTrace.endStage::" + stage + "::Finish **"
+}
+
+def logError(text){
+    echo "** pipelineTrace.logError::" + stage + "::Start **"
+
+	File f = new File(location + "/PAC.log")
+	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+    f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::End " + stage + '\r\n')
+	
+    echo "** pipelineTrace.logError::" + stage + "::Finish **"
+}
+
+return this;
