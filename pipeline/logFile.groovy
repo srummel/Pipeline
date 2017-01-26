d43bad457ab5c9a68ccdfc56719c06cafb2cf43b
Shawn Rummel <shawn.rummel@gmail.com>
2017-01-26 15:51:34 -0600

    added logging groovy file and resolved remaining path issue

---
+++ pipeline/logFile.groovy
@@ -0,0 +1,18 @@
+def copyLogFile(dir, fileToMove){
+   echo "Internal_Method::logFile.copyLogFile::Start **"
+
+//  File file = new File(fileToMove);  
+//  File dir = new File(dir);  
+//  boolean fileMoved = file.renameTo(new File(dir, file.getName()));  
+
+   echo "Internal_Method::logFile.copyLogFile::Finish **"
+}
+
+
+return this;
+
+
+
+
+
+
