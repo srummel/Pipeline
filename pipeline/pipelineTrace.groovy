649df96f3331209518cb740060dfc378bb256688
Shawn Rummel <shawn.rummel@gmail.com>
2017-01-30 12:46:07 -0600

    moved lib to com

--- pipeline/pipelineTrace.groovy
+++ pipeline/pipelineTrace.groovy
@@ -1,4 +1,4 @@
-
+package com
 
 
 @Library('pacLog')
--- src/com/pipelineTrace.groovy
+++ src/com/pipelineTrace.groovy
@@ -1,46 +1,48 @@
+package com
 
 
-
+@Library('pacLog')
 class pipelineTrace{ 
 def location
 def startStage(buildNumber, buildEnvironment, stage){
-    echo "###### pipelineTrace.startStage::" + stage + "~~location=${location}::Start ######"
+    println "###### pipelineTrace.startStage::" + stage + "~~location=${location}::Start ######"
 
 	File f = new File(location + "/PAC.log")
 	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
     f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::Begin " + stage + '\r\n')
 	
-    echo "** pipelineTrace.startStage::" + stage + "::Finish **"
+    println "** pipelineTrace.startStage::" + stage + "::Finish **"
 }
 
 
 def endStage(location, buildNumber, buildEnvironment, stage){
-    echo "** pipelineTrace.endStage::" + stage + "::Start **"
+    println "** pipelineTrace.endStage::" + stage + "::Start **"
 
 	File f = new File(location + "/PAC.log")
 	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
     f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::Complete " + stage + '\r\n')
 	
-    echo "** pipelineTrace.endStage::" + stage + "::Finish **"
+    println "** pipelineTrace.endStage::" + stage + "::Finish **"
 }
 
 def output(text){
-    echo "** pipelineTrace.endStage::" + stage + "::Start **"
+    println "** pipelineTrace.endStage::" + stage + "::Start **"
 
 	File f = new File(location + "/PAC.log")
     f.append(text + '\r\n')
 	
-    echo "** pipelineTrace.endStage::" + stage + "::Finish **"
+    println "** pipelineTrace.endStage::" + stage + "::Finish **"
 }
 
 def logError(text){
-    echo "** pipelineTrace.logError::" + stage + "::Start **"
+    println "** pipelineTrace.logError::" + stage + "::Start **"
 
 	File f = new File(location + "/PAC.log")
 	def timeStamp = new Date().format('yyyy-MM-dd HH:mm:ss.SSS')
     f.append([timeStamp, buildNumber, buildEnvironment].join("; ") + " ::End " + stage + '\r\n')
 	
-    echo "** pipelineTrace.logError::" + stage + "::Finish **"
+    println "** pipelineTrace.logError::" + stage + "::Finish **"
 }
 }
- return this;
+
+ return new pipelineTrace()
